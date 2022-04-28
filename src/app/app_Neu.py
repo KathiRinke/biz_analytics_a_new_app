399c26cad1bf8288f37645d21ef02270b42575c2
kathi <kathi.rinke@study.hs-duesseldorf.de>
2022-04-28 22:51:12 +0200

    Neue App

---
+++ src/app/app_Neu.py
@@ -0,0 +1,33 @@
+import streamlit as st
+#%%
+st.title("Vorhersage der Weltbevölkerung")
+#%%
+"Autorin: Kathi Rinke"
+#%%
+from scipy.stats import linregress
+import pandas as pd
+df = pd.read_excel('Daten_Weltbevölkerung_1960-2020_NEU.xls')
+years = df['Jahr']
+population = df['Weltbevölkerung']
+regression_result = linregress(years, population)
+scipy_slope = regression_result.slope
+scipy_intercept = regression_result.intercept
+def scipy_model(desired_year):
+    model_result = scipy_slope * desired_year + scipy_intercept
+    return model_result
+desired_year = st.number_input('Jahr', value=2021)
+prediction = scipy_model(desired_year)
+prediction_rounded = round(prediction,2)
+"Die Vorhersage der Weltbevölkerung für das ausgewählte Jahr ist:"
+st.write(prediction_rounded)
+"Millarden Einwohner weltweit"
+#%%
+st.subheader("Über das Modell und die Daten")
+"Das Modell ist ein lineares Regressionsmodell auf Grundlage von Daten von 1960 bis 2020."
+"Es steht ein Datenpunkt pro Jahr zur Verfügung."
+"Die Daten stammen aus der folgenden Quelle:"
+"The World Bank Group, 2022"
+#%%
+#%%
+
+
